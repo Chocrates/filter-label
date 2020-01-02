f31c85e3188919655539bb92b07903b5e8a91124
Chris McIntosh <chocrates@github.com>
2020-01-02 11:04:50 -0600

    Initial commit

---
+++ src/main.js
@@ -0,0 +1,21 @@
+const core = require("@actions/core")
+const github = require("@actions/github")
+
+async function run() {
+    try {
+        const validLabels = core.getInput(labels).split(',')
+        const issueLabels = github.context.payload.issue.labels.map(label => { label.name  })
+        if(!(issueLabels.filter(label => validLabels.includes(label))){
+            // stop execution
+            return 78
+        }
+    } catch(error) {
+        core.setFailed(error.message)
+    }
+}
+
+if(!module.parent) {
+    run();
+}
+
+export {run}
