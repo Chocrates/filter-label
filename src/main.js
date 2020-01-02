3b026d86e48adfc39f0589c8766ea0543c1978c3
Chris McIntosh <chocrates@github.com>
2020-01-02 11:26:26 -0600

    More debug

--- src/main.js
+++ src/main.js
@@ -5,6 +5,9 @@ async function run() {
     try {
         const validLabels = core.getInput('labels').split(',')
         const issueLabels = github.context.payload.issue.labels.map(label => { label.name  })
+        core.debug(`Valid Labels: ${validLabels}`)
+        core.debug(`Issue Labels: ${issueLabels}`)
+        core.debug(`Issue Label filter: ${issueLabels.filter(label => validLabels.includes(label))}`)
         if(!(issueLabels.filter(label => validLabels.includes(label)))){
             core.debug('Attempting to stop execution')
             // stop execution
